f8268951f14ba5a8bd1d100c54170377b5d1fc7b
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-09-11 23:23:26 +0300

    Starting somewhere, I guess.

---
+++ bonkio-gokuping.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Ping On Username Mention
+// @namespace    https://danik00111.github.io/
+// @version      0.0.1
+// @description  Will play a fnuuy sound every time your nickname is mentioned in the chat. Sound and icon directly taken from the $30 website, because I'm lazy.
+// @author       danik0011
+// @icon         https://thirtydollar.website/icons/ultrainstinct.png
+// @match        https://bonk.io/gameframe-release.html
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  let gokuHowl = new Howl({src: "https://thirtydollar.website/sounds/ultrainstinct.wav"});
+  setTimeout(function(){
+    gokuHowl.play();
+  }, 10000);
+  gokuHowl.play();
+//  var observer = new MutationObserver(function(mutations) {
+//    if (document.contains(myElement)) { // example use of the mutation observer
+//      console.log("It's in the DOM!"); // no idea how to use that
+//      observer.disconnect(); // *yawn* nah its 11pm imma do this tomorrow
+//    }
+//  });
+})();
