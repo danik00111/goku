4c9f81c5bbb78637f6b8ba80f3dad966d29be9a8
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-09-12 12:51:18 +0300

    should play it every time sus is said

--- bonkio-gokuping.user.js
+++ bonkio-gokuping.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ping On Username Mention
 // @namespace    https://danik00111.github.io/
-// @version      0.0.1
+// @version      0.0.2
 // @description  Will play a fnuuy sound every time your nickname is mentioned in the chat. Sound and icon directly taken from the $30 website, because I'm lazy.
 // @author       danik0011
 // @icon         https://thirtydollar.website/icons/ultrainstinct.png
@@ -13,14 +13,37 @@
 (function() {
   'use strict';
   let gokuHowl = new Howl({src: "https://thirtydollar.website/sounds/ultrainstinct.wav"});
-  gokuHowl.play();
-  setTimeout(function(){
-    gokuHowl.play();
-  }, 10000);
-//  var observer = new MutationObserver(function(mutations) {
-//    if (document.contains(myElement)) { // example use of the mutation observer
-//      console.log("It's in the DOM!"); // no idea how to use that
-//      observer.disconnect(); // *yawn* nah its 11pm imma do this tomorrow
-//    }
-//  });
+  // Select the target node you want to observe (in this case, the chatbox).
+const chatbox = document.getElementById('newbonklobby_chat_content');
+
+// Create a new Mutation Observer.
+const observer = new MutationObserver((mutationsList, observer) => {
+  // Loop through the mutations that have occurred.
+  for (const mutation of mutationsList) {
+    // Check if nodes were added.
+    if (mutation.type === 'childList') {
+      // Loop through the added nodes.
+      mutation.addedNodes.forEach((node) => {
+        // Check if the added node is a... uhhhh... yeah idk
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          // Read the innerHTML of the <span> element inside the <div>.
+          const message = node.querySelector('.newbonklobby_chat_msg_txt').innerHTML;
+          // Do something with the message, e.g., check if it contains the word "sus".
+          console.log(`message ${message} found`)
+          if(message.toLowerCase().contains('sus')) {
+            gokuHowl.play();
+            console.log('"sus" found');
+          }
+        }
+      });
+    }
+  }
+});
+
+// Configure the observer to watch for changes in child nodes of the chatbox.
+const config = { childList: true };
+
+// Start observing the chatbox for mutations.
+observer.observe(chatbox, config);
+
 })();
